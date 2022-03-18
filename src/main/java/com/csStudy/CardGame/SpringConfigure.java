3b6a6fb370f2d5ad1939ed0bb489e768d2e09f6a
Lairin <pdj9696@naver.com>
2022-03-18 15:36:39 +0900

    controller and etc. commit(22_03_18)

--- src/main/java/com/csStudy/CardGame/SpringConfigure.java
+++ src/main/java/com/csStudy/CardGame/SpringConfigure.java
@@ -2,6 +2,8 @@ package com.csStudy.CardGame;
 
 import com.csStudy.CardGame.mapper.CardMapper;
 import com.csStudy.CardGame.mapper.CardMapperImpl;
+import com.csStudy.CardGame.mapper.CategoryMapper;
+import com.csStudy.CardGame.mapper.CategoryMapperImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,10 @@ public class SpringConfigure {
     @Bean
     public CardMapper cardMapper() {
         return new CardMapperImpl();
+    }
+
+    @Bean
+    public CategoryMapper categoryMapper(){
+        return new CategoryMapperImpl();
     }
 }
