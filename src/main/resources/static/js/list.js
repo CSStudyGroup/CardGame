61c6d46e8a2acc2094bd46ef94e4fba85b0f162f
Lairin <pdj9696@naver.com>
2022-03-18 21:47:32 +0900

    controller and AJAX commit(22_03_18)

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -36,5 +36,28 @@ function del(test){
 }
 
 window.onload = function(){
+    const categoryList = document.getElementById("categoryList");
 
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = getCategoryList;
+
+    function getCategoryList(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                var results = httpRequest.response;
+                for (const result of results) {
+                    var op = new Option();
+                    op.value = result.cname;
+                    op.text = result.cname;
+                    categoryList.appendChild(op);
+                }
+            } else {
+                alert('Request Error!');
+            }
+        }
+    }
+
+    httpRequest.open('POST', '/card/categoryList');
+    httpRequest.responseType = "json";
+    httpRequest.send();
 }
