9f2c1e4a03ad06e690b53001b204a4e9aefaea96
Scala <scala0114@gmail.com>
2022-04-20 17:55:19 +0900

    2022_04_20_commit * add custom select box fragment * add loading page * apply custom select box to navigator/cardmain * modify cardmain design * refactor navigator.js

---
+++ src/main/resources/static/js/card_common.js
@@ -0,0 +1,13 @@
+const selectOption = function(selectBoxId, selectedOptionText, selectedOptionValue) {
+    let sb = document.querySelector(`#${ selectBoxId }`);
+    let sbOptionSelectedText = sb.querySelector('.sb-option-text');
+    sb.dataset.value = selectedOptionValue;
+    sbOptionSelectedText.textContent = selectedOptionText;
+    sb.classList.remove('sb-option-open');
+}
+
+const loadingPage = document.querySelector('.loading-page');
+window.addEventListener('load', (e) => {
+    loadingPage.style.visibility = "hidden";
+    loadingPage.style.opacity = "0";
+})
